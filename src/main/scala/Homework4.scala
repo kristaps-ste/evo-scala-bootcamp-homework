f028de15dc9651a26908f2298141846c0ea4f577
Kristaps Stebelis <63811980+kristaps-ste@users.noreply.github.com>
2020-10-03 00:13:07 +0300

    Update Homework4.scala

--- src/main/scala/Homework4.scala
+++ src/main/scala/Homework4.scala
@@ -3,12 +3,10 @@ import scala.util.Try
 
 object DataStructures {
 
-  def sortConsideringEqualValues[T: Ordering](map: Map[T, Int]): List[(Set[T], Int)] = {
-    map
-      .groupBy({ case (_, value) => value })
-      .map({ case (_, value) => value })
-      .map(_.unzip)
-      .map({case (key, value) => (key.toSeq.sorted.toSet, value.head)})
-      .toList
+ def sortConsideringEqualValues[T](map: Map[T, Int]): List[(Set[T], Int)] = {
+    val grouped = map.groupBy { case (_, value) => value }
+    val sorted = grouped.toList.sortBy { case (value, _) => value }
+    val normalized = sorted.map { case (_, value) => value.unzip }
+    normalized.map { case (k, v) => (k.toSet, v.head) }
   }
 }
